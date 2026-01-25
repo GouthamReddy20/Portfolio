55b26811469712a3d07ae4288d4083df4788086a
GouthamReddy20 <csgoutham775@gmail.com>
2026-01-26 03:25:04 +0530

    Connected frontend to live backend

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,7 +14,8 @@ function Contact() {
     setStatus("Sending...");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      // const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://portfolio-backend-mstt.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
